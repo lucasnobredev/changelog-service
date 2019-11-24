d05b17f4d4588e20fd6334d4a13c705b75d53301
Lucas Nobre <lucasnobredev@outlook.com>
2019-11-24 02:25:35 -0300

    fixing wrong names

--- src/ChangeLogService.Repository/TeamRepository.cs
+++ src/ChangeLogService.Repository/TeamRepository.cs
@@ -1,5 +1,5 @@
-﻿using ChangeLogWeb.Domain;
-using ChangeLogWeb.Domain.Interfaces;
+﻿using ChangeLogService.Domain;
+using ChangeLogService.Domain.Interfaces;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ChangeLogWeb.Repository
+namespace ChangeLogService.Repository
 {
     public class TeamRepository : ITeamRepository
     {
